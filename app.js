c65708d2d4ad9fa68f74c1fdecdec6583e2a38e7
lolyparty <ololadeolayiwola01@gmail.com>
2020-08-19 22:25:47 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-   //openweather api
+   // openweather api
+import {ApiKey} from './config.js'
 
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(showPosition); //showPosition function with parameter position
-    //     } else { 
-    //       console.log("Geolocation is not supported by this browser.");
-    //     }
-    //   }
+   const myApiKey = ApiKey.key
 
-    //   getLocation()
+    function getLocation() {
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition); //showPosition function with parameter position
+        } else { 
+          console.log("Geolocation is not supported by this browser.");
+        }
+      }
 
-    // function showPosition(position) {
-    //     var locationlat = position.coords.latitude;
-    //     var locationlong = position.coords.longitude;
-    //     console.log(locationlat, locationlong)
-    //     currentLocation()
-    // }
+      getLocation()
 
-    // async function currentLocation(){
-    // //   const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3070eaa7419d6c64f2a80da9e8f61d48`) //current weather
-    // const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=ibadan&appid=3070eaa7419d6c64f2a80da9e8f61d48`) //3 hour/5day forecast
-    //   console.log(data)
-    //   const latlong = await data.json()
-    //   console.log(latlong)
-    // }
+    function showPosition(position) {
+        var locationlat = position.coords.latitude;
+        var locationlong = position.coords.longitude;
+        console.log(locationlat, locationlong)
+        currentLocation()
+    }
+
+    async function currentLocation(){
+    //   const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=`) //current weather
+    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=ibadan&appid=${myApiKey}`) //3 hour/5day forecast
+      console.log(data)
+      const latlong = await data.json()
+      console.log(latlong)
+    }
 
 
 
@@ -49,7 +52,7 @@
     //     async function currentLocation(lat, long){
     //      try{
            
-    //         const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=3070eaa7419d6c64f2a80da9e8f61d48`) //daily and hourly forecast
+    //         const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=`) //daily and hourly forecast
 
     //           console.log(data)
     //           const latlong = await data.json()
@@ -63,10 +66,10 @@
     //      }
 
 
-    //       // const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=3070eaa7419d6c64f2a80da9e8f61d48`) //3 hour/5day forecast
-    //         // const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?q=Ibadan&appid=3070eaa7419d6c64f2a80da9e8f61d48`) //current weather 
-    //         // const data = await fetch(`http://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${long}&appid=3070eaa7419d6c64f2a80da9e8f61d48`)
+    //       // const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=`) //3 hour/5day forecast
+    //         // const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?q=Ibadan&appid=`) //current weather 
+    //         // const data = await fetch(`http://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${long}&appid=`)
     // }
 
     // weather();
-    // // 3070eaa7419d6c64f2a80da9e8f61d48
+    // // 
