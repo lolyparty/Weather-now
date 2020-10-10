237106cc1fc04de8e63f82041d1e9046b7a5caab
lolyparty <ololadeolayiwola01@gmail.com>
2020-10-10 03:41:30 +0100

    update app.js

--- app.js
+++ app.js
@@ -3,78 +3,6 @@ import { ApiKey } from './config.js';
 
 const myApiKey = ApiKey.key;
 
-// function getLocation() {
-//     if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition); //showPosition function with parameter position
-//     } else {
-//       alert("Geolocation is not supported by this browser.");
-//     }
-//   }
-
-//   getLocation()
-
-// function showPosition(position) {
-//     var locationlat = position.coords.latitude;
-//     var locationlong = position.coords.longitude;
-//     console.log(locationlat, locationlong)
-//     currentLocation(locationlat, locationlong)
-// }
-
-//     async function currentLocation(lat, long){
-//       const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myApiKey}`) //current weather
-
-//       const latlong = await data.json()
-//       console.log(latlong)
-//     }
-
-// const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=ibadan&appid=${myApiKey}&units=metric`)
-//   console.log(data)
-
-//open weather
-// const weather = function(){
-//     function showLocation() {
-//         if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition); //showPosition function with parameter position
-//         } else {
-//           console.log("Geolocation is not supported by this browser.");
-//         }
-//       }
-
-//       showLocation()
-
-//     function showPosition(position) {
-//         var locationlat = position.coords.latitude;
-//         var locationlong = position.coords.longitude;
-//         console.log(locationlat, locationlong)
-//         currentLocation(locationlat,locationlong)
-//     }
-
-//     async function currentLocation(lat, long){
-//      try{
-
-//         const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=${myApiKey}`) //daily and hourly forecast
-
-//           console.log(data)
-//           const latlong = await data.json()
-//           console.log(latlong)
-//           const weatherData = await latlong.hourly
-//           console.log(weatherData)
-//         }
-//         catch(error){
-//             console.log('error')
-//         }
-//      }
-//     }
-//     weather()
-
-//        const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${myApiKey}`) //3 hour/5day forecast
-//          const data = await fetch(`http://api.openweathermap.org/data/2.5/onecall?q=Ibadan&appid=${myApiKey}`) //current weather
-//          const data = await fetch(`http://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${long}&appid=${myApiKey}`)
-// }
-
-// weather();
-// //
-
 const domClasses = {
   date: document.querySelector('.currentdate'),
   weather: document.querySelector('.weatherstat'),
@@ -86,10 +14,6 @@ const domClasses = {
   searchLocation:document.querySelector('.searchbtn'),
   serachedInput:document.querySelector('.searchinput')
 };
-
-// domClasses.searchLocation.addEventListener('mouseover', ()=>{
-//   domClasses.searchInput.style.width = '175px'
-// })
 
 const displayDate = () => {
   const months = [
